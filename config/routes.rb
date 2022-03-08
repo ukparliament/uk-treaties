c3b8f86b16d9dbb0aa925c427295ed224f466253
Michael Smethurst <michael@smethur.st>
2022-03-08 21:14:39 +0000

    url for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  get '/' => 'home#index', :as => 'home'
+  
   get 'agreements' => 'agreement#index', :as => 'agreement_list'
   get 'agreements/:agreement' => 'agreement#show', :as => 'agreement_show'
   
