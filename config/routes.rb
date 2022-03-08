d77703d27ba0139ce2919950522127708b10f5b0
Michael Smethurst <michael@smethur.st>
2022-03-08 21:09:35 +0000

    new agreement show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get 'agreements' => 'agreement#index', :as => 'agreement_list'
+  get 'agreements/:agreement' => 'agreement#show', :as => 'agreement_show'
   
   get 'subjects' => 'subject#index', :as => 'subject_list'
   get 'subjects/:subject' => 'subject#show', :as => 'subject_show'
