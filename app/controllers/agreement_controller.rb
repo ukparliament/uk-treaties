7f655b43731f08cc4405601ad731a98c1c0047df
Michael Smethurst <michael@smethur.st>
2022-03-08 21:08:55 +0000

    new show method

--- app/controllers/agreement_controller.rb
+++ app/controllers/agreement_controller.rb
@@ -3,4 +3,9 @@ class AgreementController < ApplicationController
   def index
     @agreements = Agreement.all
   end
+  
+  def show
+    agreement = params[:agreement]
+    @agreement = Agreement.find( agreement )
+  end
 end
