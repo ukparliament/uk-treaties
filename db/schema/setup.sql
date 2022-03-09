240099970018bf9e6f6e6bde3811255b5553f8fe
Michael Smethurst <michael@smethur.st>
2022-03-09 16:52:41 +0000

    added tables for actions, action types and parties

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1,6 +1,19 @@
+drop table if exists actions;
 drop table if exists agreements;
+drop table if exists action_types;
 drop table if exists subjects;
+drop table if exists parties;
 
+create table parties (
+	id serial,
+	name varchar(255) not null,
+	primary key (id)
+);
+create table action_types (
+	id serial,
+	label varchar(255) not null,
+	primary key (id)
+);
 create table subjects (
 	id serial,
 	subject varchar(255) not null,
@@ -11,5 +24,17 @@ create table agreements (
 	title varchar(2000) not null,
 	subject_id int not null,
 	constraint fk_subject foreign key (subject_id) references subjects(id),
+	primary key (id)
+);
+create table actions (
+	id serial,
+	action_on date,
+	effective_on date,
+	action_type_id int not null,
+	agreement_id int not null,
+	party_id int not null,
+	constraint fk_action_type foreign key (action_type_id) references action_types(id),
+	constraint fk_agreement foreign key (agreement_id) references agreements(id),
+	constraint fk_party foreign key (party_id) references parties(id),
 	primary key (id)
 );
