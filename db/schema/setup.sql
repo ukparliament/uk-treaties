9f06922d00e67a4dcb6a26ff6f842fd80b52a2a1
Michael Smethurst <michael@smethur.st>
2022-03-13 22:01:12 +0000

    setup to import from json

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1,9 +1,11 @@
 drop table if exists relations;
 drop table if exists actions;
+drop table if exists records;
 drop table if exists agreements;
 drop table if exists action_types;
 drop table if exists subjects;
 drop table if exists parties;
+drop table if exists agreement_types;
 
 create table parties (
 	id serial,
@@ -16,20 +18,59 @@ create table action_types (
 	label varchar(255) not null,
 	primary key (id)
 );
+
+---- Good from here
 create table subjects (
 	id serial,
 	subject varchar(255) not null,
 	primary key (id)
 );
+
+create table agreement_types (
+	id serial,
+	short_name varchar(5) not null,
+	label varchar(12) not null,
+	primary key (id)
+);
+insert into agreement_types (short_name, label) values ( 'BI', 'Bilateral');
+insert into agreement_types (short_name, label) values ( 'MULTI', 'Multilateral');
+
 create table agreements (
 	id serial,
-	fcdo_id int not null,
-	title varchar(2000) not null,
-	description varchar(2000),
+	uuid char(36) not null,
+	agreement_id int not null,
+	agreement_type_id int,
 	subject_id int,
+	
+	
+	
+	
+	title varchar(10000) not null,
+	description varchar(10000),
+	signed_event_at varchar(255),
+	reference_values varchar(10000),
+	signed_event_on varchar(255),
+	definative_eif_event_date varchar(255),
+	country_name varchar(10000),
+	
+	constraint fk_agreement_type foreign key (agreement_type_id) references agreement_types(id),
 	constraint fk_subject foreign key (subject_id) references subjects(id),
 	primary key (id)
 );
+create table records (
+	id serial,
+	record_id varchar(255) not null,
+	agreement_id int not null,
+	constraint fk_agreement foreign key (agreement_id) references agreements(id),
+	primary key (id)
+);
+---- Good to here
+
+
+
+
+
+
 create table actions (
 	id serial,
 	action_on date,
