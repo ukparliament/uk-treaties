ec2a126e5c1bfc66faab9898b1c92212cf3c7210
Michael Smethurst <michael@smethur.st>
2022-03-15 08:36:41 +0000

    added associations to treaty parties and parties

--- app/models/treaty.rb
+++ app/models/treaty.rb
@@ -2,4 +2,9 @@ class Treaty < ApplicationRecord
   
   belongs_to :subject, optional: true
   belongs_to :treaty_type, optional: true
+  
+  has_many :treaty_parties
+  has_many :parties,
+    -> { order( 'name' ) },
+    :through => :treaty_parties
 end
