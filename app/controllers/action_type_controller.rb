38c9cb994df1174adb7396767d3ee06e7d3960c8
Michael Smethurst <michael@smethur.st>
2022-03-16 16:02:12 +0000

    new show view

--- app/controllers/action_type_controller.rb
+++ app/controllers/action_type_controller.rb
@@ -3,4 +3,9 @@ class ActionTypeController < ApplicationController
   def index
     @action_types = ActionType.all.order( 'label' )
   end
+  
+  def show
+    action_type = params[:action_type]
+    @action_type = ActionType.find( action_type )
+  end
 end
