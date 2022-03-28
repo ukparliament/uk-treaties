fff331cf72e6f839000d6a88c446b43e8c5548d6
Michael Smethurst <michael@smethur.st>
2022-03-28 09:55:29 +0100

    ordered treaties by signed_on, then in_force_on

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,6 @@ class Location < ApplicationRecord
   
   has_many :signing_locations
   has_many :treaties,
-    -> { order( 'in_force_on, signed_on' ) },
+    -> { order( 'signed_on, in_force_on' ) },
     :through => :signing_locations
 end
