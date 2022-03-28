46b2ab7cfcd422ab24c16ad90bd5c375f6df806c
Michael Smethurst <michael@smethur.st>
2022-03-28 09:57:09 +0100

    ordered treaties by signed_on, then in_force_on. order actions by action_on, then effective_on

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,9 +1,9 @@
 class Party < ApplicationRecord
 
   has_many :actions,
-    -> { order( 'action_on' ) }
+    -> { order( 'action_on, effective_on' ) }
   has_many :treaty_parties
   has_many :treaties,
-    -> { order( 'in_force_on, signed_on' ) },
+    -> { order( 'signed_on, in_force_on' ) },
   :through => :treaty_parties
 end
