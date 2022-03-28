5adea008005f42207c2a6a7d527569ae38ed15c2
Michael Smethurst <michael@smethur.st>
2022-03-28 10:13:38 +0100

    new methods for displaying action_on and effective_on

--- app/models/action.rb
+++ app/models/action.rb
@@ -3,4 +3,20 @@ class Action < ApplicationRecord
   belongs_to :treaty
   belongs_to :action_type, optional: true
   belongs_to :party
+  
+  def action_on_display
+    action_on_display = ''
+    if self.action_on
+      action_on_display = self.action_on.strftime( '%-d %B %Y')
+    end
+    action_on_display
+  end
+  
+  def effective_on_display
+    effective_on_display = ''
+    if self.effective_on
+      effective_on_display = self.effective_on.strftime( '%-d %B %Y')
+    end
+    effective_on_display
+  end
 end
