e61feafa9eee4493154bc035e558a917726f88b8
fantasticlife <michael@smethur.st:wq>
2023-03-13 12:25:01 +0000

    nested under uk-treaties for azure proxying

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get '/' => 'home#index', :as => 'home'
+  get 'uk-treaties/' => 'home#index', :as => 'home'
   
-  get 'treaties' => 'treaty#index', :as => 'treaty_list'
-  get 'treaties/:treaty' => 'treaty#show', :as => 'treaty_show'
+  get 'uk-treaties/treaties' => 'treaty#index', :as => 'treaty_list'
+  get 'uk-treaties/treaties/:treaty' => 'treaty#show', :as => 'treaty_show'
   
-  get 'treaty-types' => 'treaty_type#index', :as => 'treaty_type_list'
-  get 'treaty-types/:treaty_type' => 'treaty_type#show', :as => 'treaty_type_show'
+  get 'uk-treaties/treaty-types' => 'treaty_type#index', :as => 'treaty_type_list'
+  get 'uk-treaties/treaty-types/:treaty_type' => 'treaty_type#show', :as => 'treaty_type_show'
   
-  get 'subjects' => 'subject#index', :as => 'subject_list'
-  get 'subjects/:subject' => 'subject#show', :as => 'subject_show'
+  get 'uk-treaties/subjects' => 'subject#index', :as => 'subject_list'
+  get 'uk-treaties/subjects/:subject' => 'subject#show', :as => 'subject_show'
   
-  get 'parties' => 'party#index', :as => 'party_list'
-  get 'parties/:party' => 'party#show', :as => 'party_show'
-  get 'parties/:party/actions' => 'party#action_list', :as => 'party_action_list'
-  get 'parties/:party/treaties' => 'party#treaty_list', :as => 'party_treaty_list'
+  get 'uk-treaties/parties' => 'party#index', :as => 'party_list'
+  get 'uk-treaties/parties/:party' => 'party#show', :as => 'party_show'
+  get 'uk-treaties/parties/:party/actions' => 'party#action_list', :as => 'party_action_list'
+  get 'uk-treaties/parties/:party/treaties' => 'party#treaty_list', :as => 'party_treaty_list'
   
-  get 'action-types' => 'action_type#index', :as => 'action_type_list'
-  get 'action-types/:action_type' => 'action_type#show', :as => 'action_type_show'
+  get 'uk-treaties/action-types' => 'action_type#index', :as => 'action_type_list'
+  get 'uk-treaties/action-types/:action_type' => 'action_type#show', :as => 'action_type_show'
   
-  get 'locations' => 'location#index', :as => 'location_list'
-  get 'locations/:location' => 'location#show', :as => 'location_show'
+  get 'uk-treaties/locations' => 'location#index', :as => 'location_list'
+  get 'uk-treaties/locations/:location' => 'location#show', :as => 'location_show'
   
-  get 'colophon' => 'colophon#index', :as => 'colophon_list'
-  get 'privacy-policy' => 'privacy#index', :as => 'privacy_list'
+  get 'uk-treaties/colophon' => 'colophon#index', :as => 'colophon_list'
+  get 'uk-treaties/privacy-policy' => 'privacy#index', :as => 'privacy_list'
 end
